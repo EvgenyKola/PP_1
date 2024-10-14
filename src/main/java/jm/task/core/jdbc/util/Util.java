56aabf3b40eba5e1d826cc8138e19c23f10f87dc
Evgeny K <KorzenokEv@gmail.com>
2024-10-14 20:51:45 +0300

    C&P

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,7 +4,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
     private static final String URL = "jdbc:mysql://korzened.beget.tech:3306/korzened_kata?useSSL=false";
     private static final String USER = "korzened_kata";
     private static final String PASSWORD = "Q1S2C3q1w2e3r4t5y6";
@@ -12,11 +11,8 @@ public class Util {
     public static Connection getConnection() {
         Connection conn = null;
         try {
-            // Загружаем драйвер
             Class.forName("com.mysql.cj.jdbc.Driver");
-            // Получаем соединение
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            //System.out.println("Подключение к базе данных успешно выполнено!");
         } catch (SQLException e) {
             System.err.println("Ошибка подключения: " + e.getMessage());
         } catch (ClassNotFoundException e) {
